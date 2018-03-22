f5b9572cb45e103104a643c37eae5e5bcaad3137
Максим Лозовский <mak2903@tut.by>
2018-03-22 22:17:00 +0300

    Добавил половину части 2

--- MainGameProject/Program.cs
+++ MainGameProject/Program.cs
@@ -23,7 +23,7 @@ namespace GameMain
     public class Player : IComparable<Player>  // класс персонажа
     {
         // Характеристики персонажа
-        protected readonly Guid PlayerID;
+        protected readonly Guid PlayerID; // ID персонажа
         protected readonly string PlayerName; // Имя персонажа
         public string PlayerCondition; // состояние персонажа
         protected readonly string PlayerRace; //раса персонажа
@@ -79,12 +79,13 @@ namespace GameMain
 
         public void ApplyHeal(int heal)  // пополнение здоровья
         {
-            if (PlayerCurrentMana / 2 >= heal)
-                PlayerCurrentHealth += heal;
-            if (PlayerCurrentHealth > PlayerMaxHealth)
-            {
-                PlayerCurrentHealth = PlayerMaxHealth;
-            }
+             if (PlayerCurrentMana / 2 >= heal)
+                 PlayerCurrentHealth += heal;
+             if (PlayerCurrentHealth > PlayerMaxHealth)
+             {
+                 PlayerCurrentHealth = PlayerMaxHealth;
+             }
+
 
         }
         public void UseSkill(int UseMana)   // использование маны и проверка на ее кол-во
@@ -99,6 +100,101 @@ namespace GameMain
 
 
     }
+
+    public interface IMagic  // интерфейс волшебство
+    {
+        void UseSkill(MagicPlayer person , int damage = 0);     
+    }
+
+    public abstract class Skill : IMagic
+    {
+        public int SkillMinMana { get; set; } // минимальное значение маны
+        public int SkillCast { get; set; } // вербальное использование скилла
+        public int SkillAction { get; set; } // движение при использовании скилла 
+
+        public abstract void UseSkill(MagicPlayer person, int damage = 0);
+    }
+
+    // классы заклинаний :
+
+    public class RegenHealth : Skill // Добавление здоровья
+    {
+        public int PlusHealth { get; set; } // минимальное значение маны
+        public override void UseSkill (MagicPlayer person, int heal = 0)
+        {
+            heal = PlusHealth;
+            SkillMinMana = PlusHealth * 2;
+            person.UseSkill(SkillMinMana);
+            if (person.PlayerCurrentMana / 2 >= PlusHealth)
+                person.PlayerCurrentMana += PlusHealth;
+            if (person.PlayerCurrentHealth > person.PlayerMaxHealth)
+            {
+                person.PlayerCurrentHealth = person.PlayerMaxHealth;
+            }
+        }
+    }
+
+    public class Heal : Skill // Вылечить
+    {
+        public override void UseSkill(MagicPlayer person, int damage = 0)
+        {
+            SkillMinMana = 20;
+            person.UseSkill(SkillMinMana);
+            if (person.PlayerCondition == "болен")
+                person.PlayerCondition = "здоров";
+        }
+    }
+
+    public class Antidote : Skill // противоядие
+    {
+        public override void UseSkill(MagicPlayer person, int damage = 0)
+        {
+            SkillMinMana = 30;
+            person.UseSkill(SkillMinMana);
+            if (person.PlayerCondition == "отравлен")
+                person.PlayerCondition = "здоров";
+        }
+    }
+
+    public class Reincarnation : Skill // возродить/оживить
+    {
+        public override void UseSkill(MagicPlayer person, int damage = 0)
+        {
+            SkillMinMana = 150;
+            person.UseSkill(SkillMinMana);
+            if (person.PlayerCondition == "мертв")
+            {
+                person.PlayerCondition = "ослаблен";
+                person.PlayerCurrentHealth = 1;
+            }
+        }
+    }
+
+    public class Armor : Skill // броня ( сделать неуязвимым )
+    {
+        public override void UseSkill(MagicPlayer person, int time = 0)
+        {
+            SkillMinMana = 50 * time;
+            person.UseSkill(SkillMinMana);
+            // тут работа со временем, обработать в UNITY!!!!!
+        }
+    }
+
+    public class Paralyzed : Skill // отомри
+    {
+        public override void UseSkill(MagicPlayer person, int damage = 0)
+        {
+            SkillMinMana = 85;
+            person.UseSkill(SkillMinMana);
+            if (person.PlayerCondition == "парализован")
+            {
+                person.PlayerCondition = "здоров";
+                person.PlayerCurrentHealth = 1;
+            }
+        }
+    }
+
+
     public class Item
     {
 
